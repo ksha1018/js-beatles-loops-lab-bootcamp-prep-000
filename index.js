401972b5a8010af5f1a9f55e3220eeb4045f1b0c
ksha1018@naver.com <ksha1018@naver.com>
2018-03-03 01:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(array1, array2){
   for(var i=0; i<array1.length; i++){
     array3[i] = array1[i] + " plays " + array2[i];
   }
-  return arrays;
+  return array3;
 }
 
 console.log(theBeatlesPlay(musicians,instruments));

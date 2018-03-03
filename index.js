2bdbb9c4439a99b6be03540faf3f380c79c1f102
ksha1018@naver.com <ksha1018@naver.com>
2018-03-03 01:20:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+    
+function theBeatlesPlay(array1, array2){
+  var array3 =[];
+  for(var i=0; i<array1.length; i++){
+    array3[i] = array1[i] + " plays " + array2[i];
+  }
+  return arrays;
+}
+
+console.log(theBeatlesPlay(musicians,instruments));
+
+    
